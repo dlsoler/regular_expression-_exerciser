9cec2a1e5f69d59ebd759a9085b51e961c742954
Diego Luis Soler <solerdiego@gmail.com>
2022-02-18 22:18:59 -0300

    Initial commit from me

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,239 @@
 import * as React from "react"
+import { Box, Button, CheckBoxGroup, Form, FormField, Grommet, Header, Heading, Select, Text, TextArea, TextInput } from 'grommet';
+// import { Home } from 'grommet-icons';
+import styled from "styled-components";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+    colors: {
+      brand: '#002f8e'
+    }
   },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
+};
+
+
+const AppForm = styled(Form)`
+width: 100%;
+max-width: 640px;
+`
+
+const flagOptions = [
+  "d: Generate indices for substring matches.",
+  "g: Global search.",
+  "i: Case-insensitive search.",
+  "m: Multi-line search.",
+  "s: Allows . to match newline characters.",
+  "u: \"unicode\"; treat a pattern as a sequence of unicode code points.",
+  "y: Perform a \"sticky\" search that matches starting at the current position in the target string."
 ]
 
-// markup
+const funcOptions = [
+  'exec',
+  'test',
+  'match',
+  'matchAll',
+  'search',
+  'replace',
+  'replaceAll',
+  'split'
+];
+
+function objToString(obj) {
+  if (typeof obj !== 'object') {
+    return obj;
+  }
+  const keys = Object.keys(obj);
+  let res = '{';
+  keys.forEach((key) => {
+    if (typeof obj[key] !== obj) {
+      res += `\n    ${key}: ${JSON.stringify(obj[key])}`;
+    } else {
+      res += `\n${objToString(obj[key])}`;
+    }
+  });
+  res += '\n}\n';
+  return res;
+}
+
 const IndexPage = () => {
+  const pattern = useFormInput('');
+  const [flags, setFlags] = React.useState([flagOptions[1]]);
+  const text = useFormInput('');
+  const [func, setFunc] = React.useState('exec');
+  const newSubstr = useFormInput('');
+  const [disableNewSubstr, setDisableNewSubstr ] = React.useState(true);
+  const warnings = useFormInput('');
+  
+  const result = useFormInput('');
+  
+
+  function onClickRun() {
+    const chars = flags.map((item) => item.charAt(0));
+
+    if ((func === 'matchAll' || func === 'replaceAll') && chars.indexOf('g') < 0) {
+      warnings.setValue('The methods matchAll and replaceAll require the flag "g" is activated');
+      setFlags(flags.concat([flagOptions[1]]));
+      chars.push('g');
+    } else {
+      warnings.setValue('');
+    }
+    const flagsStr = chars.join('');
+    const regex = new RegExp(pattern.value, flagsStr);
+
+    let res;
+    switch(func) {
+      case 'exec':
+      case 'test':
+        res = regex[func](text.value);
+        break;
+      case 'match':
+      case 'search':
+      case 'split':
+        res = text.value[func](regex);
+        break;
+      case 'matchAll':
+        res = Array.from(text.value[func](regex));
+        break;
+      case 'replace':
+      case 'replaceAll':
+        res = text.value[func](regex, newSubstr.value);
+        break;
+      default:
+        throw new Error('Something was absolutely wrong!');
+    }
+    const resStr = objToString(res);
+    result.setValue(resStr);
+  }
+
+  function onChangeFunc(option) {
+    setFunc(option);
+    if (option === 'replace' || option === 'replaceAll') {
+      setDisableNewSubstr(false);
+    } else {
+      setDisableNewSubstr(true);
+    }
+  }
+
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <Grommet theme={theme}>
+      <main >
+        <Header background="brand">
+          <Heading alignSelf="start" level="2" margin="small">Javascript Regular Expression Exerciser</Heading>
+        </Header>
+        <Box
+          tag="div"
+          direction="column"
+          align="center"
+          justify="between"
+          background="light-2" 
+          pad={{ vertical: 'small', horizontal: 'medium' }}
+          elevation="medium"
+        >
+          <AppForm id="reg-exp-form">
+            
+            <FormField
+              label="Regular expression:"
+              htmlFor="reg-exp-id"
+            >
+              <TextInput
+                id="reg-exp-pattern"
+                name="pattern"
+                {...pattern}
+                placeholder="Regular expression pattern"
+              />
+            </FormField>
+
+            <FormField label="Flags" htmlFor="reg-exp-flags">
+              <CheckBoxGroup
+                id="reg-exp-flags"
+                options={flagOptions}
+                value={flags}
+                onChange={event => setFlags(event.value)}
+              />
+            </FormField>
+
+            <FormField label="RegEx Method" htmlFor="reg-exp-func">
+              <Select
+                options={funcOptions}
+                value={func}
+                onChange={({ option }) => onChangeFunc(option)}
+              />
+            </FormField>
+
+            <FormField label="Text" htmlFor="text">
+              <TextArea
+                id="text"
+                {...text}
+                placeholder="Text to search into"
+              />
+            </FormField>
+
+            <FormField label="New substring for replacement" htmlFor="newSubstr">
+              <TextArea
+                id="newSubstr"
+                {...newSubstr}
+                placeholder="New Substring"
+                disabled={disableNewSubstr}
+              />
+            </FormField>
+
+            <FormField label="Warnings" htmlFor="warnings">
+              <TextArea
+                id="warnings"
+                {...warnings}
+                disabled
+              />
+            </FormField>
+
+            <FormField label="Result" htmlFor="text">
+              <TextArea
+                id="result"
+                {...result}
+                placeholder="Result"
+              />
+            </FormField>
+            <Box
+              direction="row"
+              gap="medium"
+              justify="center"
+              pad={{ vertical: 'large', horizontal: 'small'}}
+            >
+              <Button primary label="Run" onClick={() => onClickRun()}/>
+            </Box>
+          </AppForm>
+        </Box>
+        <Box
+          tag="footer"
+          direction="row"
+          align="center"
+          alignSelf="center"
+          pad={{ vertical: 'large', horizontal: 'medium' }}
+          elevation="medium"
+        >
+          <Text>Powered by <a href="https://reactjs.org/" title="React">React</a>, <a href="https://www.gatsbyjs.com/" title="Gatsby">Gatsby</a> and <a href="https://grommet.io" title="Grommet">Grommet</a></Text>
+        </Box>
+      </main>
+    </Grommet>
   )
+}
+
+function useFormInput(initialValue) {
+  const [value, setValue] = React.useState(initialValue);
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return {
+    value,
+    setValue,
+    onChange: handleChange
+  };
 }
 
 export default IndexPage
